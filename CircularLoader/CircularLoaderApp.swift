2a4c0df769521a2efb58448edaccc9c011acb8b7
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-15 23:59:50 +0200

    Initial Commit

---
+++ CircularLoader/CircularLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularLoaderApp.swift
+//  CircularLoader
+//
+//  Created by Pouya Sadri on 15/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CircularLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
